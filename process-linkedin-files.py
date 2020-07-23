35a16309258261915e032b5058942ef764201731
krathor <kuldeepsinghlive@gmail.com>
2020-07-23 01:48:21 -0600

    Updated for exporting files with hiring managers and related profiles separately

--- process-linkedin-files.py
+++ process-linkedin-files.py
@@ -1,38 +1,36 @@
-import requests
 from bs4 import BeautifulSoup
-import re
-import json
 import glob
+import pandas as pd
 
 hiring_profiles = {}
 related_profiles = {}
 
 for file in glob.glob('Files/*'):
-    #print(file)
     with open(file, 'rb') as fp: # Pass an encoded byte string 'rb' to avoid UnicodeDecodeError
         soup = BeautifulSoup(fp, 'html.parser')
-        # print(soup)
         for li in soup.find_all("li", "pv-browsemap-section__member-container pv-browsemap-section__member-container-line ember-view"):
             for a in li.find_all("a"):
-                print(a.get("href"))
+                # print(a.get("href"))
                 profile_link = a.get("href")
             for div in li.find_all("div", "pv-browsemap-section__member-detail"):
                 for span in div.find_all("span", "name"):
-                    print(span.string)
+                    # print(span.string)
                     profile_name = span.string
                 for p in div.find_all("p", "pv-browsemap-section__member-headline t-14 t-black t-normal"):
-                    print(p.div.string)
+                    # print(p.div.string)
                     profile_heading = p.div.string
 
-            if "hiring" in profile_heading.lower():
+            if "hiring" in profile_heading.lower() or "hiring" in profile_name.lower():
                 hiring_profiles[profile_name] = {}
-                hiring_profiles[profile_name]['Link '] = profile_link
-                hiring_profiles[profile_name]['Heading '] = profile_heading
+                hiring_profiles[profile_name]['Profile Link '] = profile_link
+                hiring_profiles[profile_name]['Heading '] = profile_heading.strip()
             else:
                 related_profiles[profile_name] = {}
-                related_profiles[profile_name]['Link '] = profile_link
-                related_profiles[profile_name]['Heading '] = profile_heading
+                related_profiles[profile_name]['Profile Link '] = profile_link
+                related_profiles[profile_name]['Heading '] = profile_heading.strip()
 
-print(len(hiring_profiles))
-print("-----------")
-print(len(related_profiles))
+df_hiring = pd.DataFrame.from_dict(hiring_profiles, orient='index') # convert dict to dataframe
+df_hiring.to_csv('Hiring profiles.csv')
+
+df_related = pd.DataFrame.from_dict(related_profiles, orient='index') # convert dict to dataframe
+df_related.to_csv('Related profiles.csv')
